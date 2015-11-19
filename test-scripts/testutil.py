7c0ff0d5f4ef47db8594a2dc1330dd3d6957bb11
Erlend Graff <erlend.graff@gmail.com>
2015-11-20 00:43:02 +0100

    Moved and refactored test framework, so it can be shared across multiple classes

---
+++ test-scripts/testutil.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+
+import os
+import re
+import subprocess
+
+import testenv
+import asynclib
+
+GS = testenv.getGhostScript()
+CMP = testenv.getCompare()
+PDFINFO = testenv.getPDFInfo()
+
+
+def mkdirp(path):
+  try:
+    os.makedirs(path)
+  except OSError:
+    if not os.path.isdir(path):
+      raise
+
+
+def _convertPdfPageToPngAsync(pdfPath, pageNum, outputPngPath):
+  gsCmd = [
+            GS, '-q', '-dQUIET', '-dSAFER', '-dBATCH', '-dNOPAUSE',
+            '-dNOPROMPT', '-sDEVICE=pngalpha', '-dMaxBitmap=500000000',
+            '-dAlignToPixels=0', '-dGridFitTT=2', '-r150',
+            '-o', outputPngPath, '-dFirstPage=%s' % pageNum,
+            '-dLastPage=%s' % pageNum, pdfPath
+          ]
+
+  task = asynclib.AsyncPopen(gsCmd, shell=False, env=os.environ, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+  return task
+
+
+class ComparePngsAsyncTask(asynclib.AsyncTask):
+  def __init__(self, pngPathFirst, pngPathSecond, outputDiffPath):
+    cmpCmd = [CMP, '-metric', 'ae', pngPathFirst, pngPathSecond, outputDiffPath]
+    self.__cmpProc = subprocess.Popen(cmpCmd, shell=False, env=os.environ, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+  def wait(self):
+    self.__cmpProc.wait()
+    lines = self.__cmpProc.stderr.readlines()
+    assert self.__cmpProc.returncode <= 1
+
+    # Needed because lines[0] could be something like "1.33125e+006"
+    self.__result = int(float(lines[0]))
+
+  # Result is diff (0 means equal)
+  @property
+  def result(self):
+    return self.__result
+
+
+class PdfFile(object):
+  def __init__(self, path):
+    self.path = path
+
+    self.__determineNumPagesInPdf()
+
+  def __determineNumPagesInPdf(self):
+    # use pdfinfo to extract number of pages in pdf file
+    output = subprocess.check_output([PDFINFO, self.path])
+    pages = re.findall(r"\d+", re.search(r"Pages:.*", output).group())[0]
+
+    self.__numPages = int(pages)
+
+  def numPhysicalPages(self):
+    return self.__numPages
+
+  # Generate PNG for given page number in PDF
+  def getPngForPageAsync(self, pageNum, outputPngPath):
+    assert pageNum >= 1
+    assert pageNum <= self.numPhysicalPages()
+
+    return _convertPdfPageToPngAsync(self.path, pageNum, outputPngPath)
+
+  # Generate PNG for given page number in PDF
+  def getPngForPage(self, pageNum, outputPngPath, callback):
+    task = self.getPngForPageAsync(pageNum, outputPngPath)
+    task.await(callback)
