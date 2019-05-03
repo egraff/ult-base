a41c87916f5e8d370cf273e45e1f8b89ababb435
Erlend Graff <erlend.graff@gmail.com>
2019-05-04 01:17:44 +0200

    AppVeyor: fix ProTeXt build

--- ci/appveyor_install_protext2018.ps1
+++ ci/appveyor_install_protext2018.ps1
@@ -35,11 +35,30 @@ if ($exitCode -ne 0)
 
 $protextDir = [string](Resolve-Path "$scriptRoot\protext")
 
-. "${protextDir}\MiKTeX\setup\setup-2.9.6406-x64.exe" `
-  --unattended `
-  --shared `
-  --package-set=complete `
-  --install-from-local-repository `
-  --local-package-repository="${protextDir}\MiKTeX\tm\packages" `
-  --auto-install=yes `
-  --paper-size=A4
+$pinfo = New-Object System.Diagnostics.ProcessStartInfo
+$pinfo.FileName = "${protextDir}\MiKTeX\setup\setup-2.9.6406-x64.exe"
+$pinfo.Arguments = "--unattended --shared --package-set=complete --install-from-local-repository --local-package-repository='${protextDir}\MiKTeX\tm\packages' --auto-install=yes --paper-size=A4"
+$pinfo.RedirectStandardError = $true
+$pinfo.RedirectStandardOutput = $true
+$pinfo.UseShellExecute = $false
+$pinfo.WorkingDirectory = $scriptRoot
+
+Write-Host "Starting MiKTeX installer"
+$p = New-Object System.Diagnostics.Process
+$p.StartInfo = $pinfo
+$p.Start() | Out-Null
+
+Write-Host "Waiting for MiKTeX installer to finish..."
+
+# Start async reads of output streams to avoid deadlock
+$p.BeginOutputReadLine()
+$p.BeginErrorReadLine()
+
+$p.WaitForExit()
+$exitCode = $p.ExitCode
+Write-Host "MiKTeX installer exited with code $exitCode"
+
+if ($exitCode -ne 0)
+{
+  throw "MiKTeX installer failed"
+}
