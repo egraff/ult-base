57730653d8c9cfaec9e07d4e274f76ac01d82664
Erlend Graff <erlend.graff@gmail.com>
2019-05-05 03:16:03 +0200

    AppVeyor: try to fix MiKTeX install

--- ci/appveyor_install_miktex-latest.ps1
+++ ci/appveyor_install_miktex-latest.ps1
@@ -2,35 +2,32 @@ $ErrorActionPreference = "Stop"
 
 $scriptRoot = (Resolve-Path $(If ($PSScriptRoot) { $PSScriptRoot } Else { "." })).Path
 
-$setupZipPath = "$scriptRoot\miktexsetup.zip"
-(New-Object System.Net.WebClient).DownloadFile('http://ftp.rrze.uni-erlangen.de/ctan/systems/win32/miktex/setup/windows-x64/miktexsetup-2.9.6942-x64.zip', $setupZipPath)
+$installerPath = "$scriptRoot\basic-miktex-x64.exe"
+(New-Object System.Net.WebClient).DownloadFile('http://mirrors.rit.edu/CTAN/systems/win32/miktex/setup/windows-x64/basic-miktex-2.9.7031-x64.exe', $installerPath)
 
-Expand-Archive $setupZipPath -DestinationPath $scriptRoot -Force
+$pinfo = New-Object System.Diagnostics.ProcessStartInfo
+$pinfo.FileName = $installerPath
+$pinfo.Arguments = "--unattended --shared --auto-install=yes --paper-size=A4"
+$pinfo.RedirectStandardError = $true
+$pinfo.RedirectStandardOutput = $true
+$pinfo.UseShellExecute = $false
+$pinfo.WorkingDirectory = $scriptRoot
 
-$pkgDir = md "miktex-packages" -Force | %{ $_.FullName }
+Write-Host "Starting MiKTeX installer"
+$p = New-Object System.Diagnostics.Process
+$p.StartInfo = $pinfo
+$p.Start() | Out-Null
 
-. "$scriptRoot\miktexsetup.exe" `
-  download `
-  --package-set=complete `
-  --local-package-repository="$pkgDir" `
-  --remote-package-repository=http://ftp.rrze.uni-erlangen.de/ctan/systems/win32/miktex/tm/packages/ `
-  --verbose
+Write-Host "Waiting for MiKTeX installer to finish..."
 
-$exitCode = $LastExitCode
-if ($exitCode -ne 0)
-{
-  throw "Failed to download MiKTeX packages"
-}
+# Start async reads of output streams to avoid deadlock
+$p.BeginOutputReadLine()
+$p.BeginErrorReadLine()
 
-. "$scriptRoot\miktexsetup.exe" `
-  install `
-  --shared `
-  --modify-path `
-  --package-set=complete `
-  --local-package-repository="$pkgDir" `
-  --verbose
+$p.WaitForExit()
+$exitCode = $p.ExitCode
+Write-Host "MiKTeX installer exited with code $exitCode"
 
-$exitCode = $LastExitCode
 if ($exitCode -ne 0)
 {
   throw "MiKTeX installer failed"
@@ -42,19 +39,11 @@ $env:Path = [System.Environment]::ExpandEnvironmentVariables(
 )
 refreshenv
 
-$ErrorActionPreference = "Continue"
-
-mpm --admin --update-db
-# mpm --admin --upgrade --package-level=basic
-# mpm --admin --find-updates | foreach { $_.ToString() } | select-string "^(?!miktex.*$)" > upd-packages.txt
-# mpm --admin --update-some=upd-packages.txt --verbose
+mpm --admin --update-db --repository=http://ftp.rrze.uni-erlangen.de/ctan/systems/win32/miktex/tm/packages/ --verbose
+mpm --admin --upgrade --package-level=basic --repository=http://ftp.rrze.uni-erlangen.de/ctan/systems/win32/miktex/tm/packages/ --verbose
+mpm --admin --update --repository=http://ftp.rrze.uni-erlangen.de/ctan/systems/win32/miktex/tm/packages/ --verbose
 
 initexmf --admin --enable-installer --verbose
 initexmf --admin --default-paper-size=a4 --verbose
 initexmf --admin --update-fndb --verbose
 initexmf --admin --mkmaps --verbose
-
-initexmf --enable-installer --verbose
-initexmf --default-paper-size=a4 --verbose
-initexmf --update-fndb --verbose
-initexmf --mkmaps --verbose
