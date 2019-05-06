bd754bdc16ce6a549dff79c9da61d706f1767a45
Erlend Graff <erlend.graff@gmail.com>
2019-05-06 14:41:44 +0200

    AppVeyor: fix TL builds

--- ci/appveyor_install_texlive-2017.ps1
+++ ci/appveyor_install_texlive-2017.ps1
@@ -1,6 +1,6 @@
 $ErrorActionPreference = "Stop"
 
 $scriptRoot = (Resolve-Path $(If ($PSScriptRoot) { $PSScriptRoot } Else { "." })).Path
-. .\install_texlive_windows.ps1 `
+. "${scriptRoot}\install_texlive_windows.ps1" `
     -Repository 'http://ftp.math.utah.edu/pub/tex/historic/systems/texlive/2017/tlnet-final' `
     -ProfilePath "${scriptRoot}\..\texlive-win.profile"
--- ci/appveyor_install_texlive-latest.ps1
+++ ci/appveyor_install_texlive-latest.ps1
@@ -1,6 +1,6 @@
 $ErrorActionPreference = "Stop"
 
 $scriptRoot = (Resolve-Path $(If ($PSScriptRoot) { $PSScriptRoot } Else { "." })).Path
-. .\install_texlive_windows.ps1 `
+. "${scriptRoot}\install_texlive_windows.ps1" `
     -Repository 'http://mirrors.rit.edu/CTAN/systems/texlive/tlnet' `
     -ProfilePath "${scriptRoot}\..\texlive-win.profile"
