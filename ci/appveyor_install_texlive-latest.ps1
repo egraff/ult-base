15af72a9b3a9271bb623d8f61cc0f103500c3405
Erlend Graff <erlend.graff@gmail.com>
2019-11-08 16:48:22 +0100

    Some CI restructuring

--- ci/appveyor_install_texlive-latest.ps1
+++ ci/appveyor_install_texlive-latest.ps1
@@ -3,4 +3,4 @@ $ErrorActionPreference = "Stop"
 $scriptRoot = (Resolve-Path $(If ($PSScriptRoot) { $PSScriptRoot } Else { "." })).Path
 . "${scriptRoot}\install_texlive_windows.ps1" `
     -Repository 'http://mirrors.rit.edu/CTAN/systems/texlive/tlnet' `
-    -ProfilePath "${scriptRoot}\..\texlive-win.profile"
+    -ProfilePath "${scriptRoot}\texlive2019-win.profile"
