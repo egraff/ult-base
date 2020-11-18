4dc4d5c2bebab198f6c04f360db38d1648864c4a
Erlend Graff <erlend.graff@gmail.com>
2020-11-18 08:59:34 +0100

    CI: try to install latest MacTeX without updating

--- ci/travis_install_osx-latest.sh
+++ ci/travis_install_osx-latest.sh
@@ -5,16 +5,20 @@ set -e
 
 TLNET_REPO="http://mirrors.rit.edu/CTAN/systems/texlive/tlnet"
 
-brew cask install basictex
+#brew cask install basictex
+wget http://mirrors.rit.edu/CTAN/systems/mac/mactex/mactex-basictex-20200407.pkg
+sudo installer -verbose -pkg ./mactex-basictex-20200407.pkg -target /
+
 # export PATH=/Library/TeX/Distributions/.DefaultTeX/Contents/Programs/texbin:$PATH
 export PATH=/Library/TeX/Distributions/Programs/texbin:$PATH
 
 # See https://tug.org/pipermail/tex-live/2020-May/045610.html
-wget ${TLNET_REPO}/update-tlmgr-latest.sh
-sudo sh ./update-tlmgr-latest.sh
+#wget ${TLNET_REPO}/update-tlmgr-latest.sh
+#sudo sh ./update-tlmgr-latest.sh
 
 sudo tlmgr option repository ${TLNET_REPO}
-sudo -i tlmgr update --self --all
+#sudo -i tlmgr update --self --all
+sudo -i tlmgr update --all
 sudo tlmgr install            \
   collection-basic            \
   collection-bibtexextra      \
