9000568a31f5e59a8985b1c48114591472bd5873
Erlend Graff <erlend.graff@gmail.com>
2022-09-17 18:12:56 +0200

    CI: ditch Travis

--- ci/travis_install_linux-latest.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Exit on failure, verbose
-set -ev
-
-SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
-. ${SCRIPT_DIR}/install_texlive_linux.sh http://mirrors.rit.edu/CTAN/systems/texlive/tlnet ${SCRIPT_DIR}/texlive2021.profile
-
-sudo -i tlmgr update --self --all
--- ci/travis_install_linux-tl2018.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# Exit on failure, verbose
-set -ev
-
-SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
-. ${SCRIPT_DIR}/install_texlive_linux.sh http://ftp.math.utah.edu/pub/tex/historic/systems/texlive/2018/tlnet-final ${SCRIPT_DIR}/texlive2018.profile
