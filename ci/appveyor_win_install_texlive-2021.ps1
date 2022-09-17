2a6fb448f39b383e1a23e32426abecf862fcb8c2
Erlend Graff <erlend.graff@gmail.com>
2022-09-17 18:08:00 +0200

    CI/AppVeyor: add missing TL 2021 install script

---
+++ ci/appveyor_win_install_texlive-2021.ps1
@@ -0,0 +1,6 @@
+$ErrorActionPreference = "Stop"
+
+$scriptRoot = (Resolve-Path $(If ($PSScriptRoot) { $PSScriptRoot } Else { "." })).Path
+. "${scriptRoot}\install_texlive_windows.ps1" `
+    -Repository 'http://ftp.math.utah.edu/pub/tex/historic/systems/texlive/2021/tlnet-final' `
+    -ProfilePath "${scriptRoot}\texlive2021-win.profile"
