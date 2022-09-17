d025d6a6f69107763283b8c29e4f8d6e3527e6a2
Erlend Graff <erlend.graff@gmail.com>
2022-09-17 14:48:43 +0200

    CI/AppVeyor: move install instructions to separate sh script

---
+++ ci/appveyor_linux_install_texlive-latest.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Exit on failure, verbose
+set -ev
+
+SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
+. ${SCRIPT_DIR}/install_texlive_linux.sh http://mirrors.rit.edu/CTAN/systems/texlive/tlnet ${SCRIPT_DIR}/texlive2022.profile
+
+sudo -i tlmgr update --self --all
