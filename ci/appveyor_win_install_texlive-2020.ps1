f8aad9fafe283f456dbcb961a902c953d6871bb6
Erlend Graff <erlend.graff@gmail.com>
2023-03-25 23:43:25 +0100

    CI/AppVeyor: try to use HTTPS for TL repositories

--- ci/appveyor_win_install_texlive-2020.ps1
+++ ci/appveyor_win_install_texlive-2020.ps1
@@ -2,5 +2,5 @@ $ErrorActionPreference = "Stop"
 
 $scriptRoot = (Resolve-Path $(If ($PSScriptRoot) { $PSScriptRoot } Else { "." })).Path
 . "${scriptRoot}\install_texlive_windows.ps1" `
-    -Repository 'http://ftp.math.utah.edu/pub/tex/historic/systems/texlive/2020/tlnet-final' `
+    -Repository 'https://ftp.math.utah.edu/pub/tex/historic/systems/texlive/2020/tlnet-final' `
     -ProfilePath "${scriptRoot}\texlive2020-win.profile"
