3f255679ab1a1c06b0d5d6105024aea1015362ba
Erlend Graff <erlend.graff@gmail.com>
2023-04-02 11:41:43 +0200

    CI/AppVeyor: try to manually install currfile for MiKTeX

--- ci/appveyor_win_postinstall_miktex-latest.ps1
+++ ci/appveyor_win_postinstall_miktex-latest.ps1
@@ -1,3 +1,6 @@
 $ErrorActionPreference = "Stop"
 
 initexmf --admin --update-fndb --verbose
+
+# Hack to try and work around problem auto-installing currfile
+miktex --admin --verbose packages install currfile
